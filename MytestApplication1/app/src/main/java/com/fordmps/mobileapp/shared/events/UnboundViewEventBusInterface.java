1f37380a414942dc595d0cf8244765f53b704430
LincolnWay <LincolnWay@MGC12ZG231JV40.fbpld77.ford.com>
2020-08-13 11:01:02 -0400

    Fix Unboundeventbus import

--- MytestApplication1/app/src/main/java/com/fordmps/mobileapp/shared/events/UnboundViewEventBusInterface.java
+++ MytestApplication1/app/src/main/java/com/fordmps/mobileapp/shared/events/UnboundViewEventBusInterface.java
@@ -1,5 +1,7 @@
 package com.fordmps.mobileapp.shared.events;
 
+import com.fordmps.mobileapp.move.StartActivityEvent;
+
 public interface UnboundViewEventBusInterface {
     void send(StartActivityEvent event);
 }
