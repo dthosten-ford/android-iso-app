32b0aaffa3c38a3920889eea9cc2f7e8d61ad675
kjohn414-ford <kjohn414@ford.com>
2020-08-13 12:11:44 -0400

    vehicleHealthAlertsUtil

---
+++ MytestApplication1/app/src/main/java/com/fordmps/mobileapp/move/vehiclehealthalerts/VehicleHealthAlertsUtil.java
@@ -0,0 +1,10 @@
+package com.fordmps.mobileapp.move.vehiclehealthalerts;
+
+import com.ford.dashboard.models.VehicleInfo;
+
+public class VehicleHealthAlertsUtil {
+
+    public static String getVhaSource(VehicleInfo vehicleInfo) {
+        return "kelly";
+    }
+}
