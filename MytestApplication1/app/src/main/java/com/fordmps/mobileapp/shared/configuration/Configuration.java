c127f5d5fd0cf5da0b6e82c3e8bbd7d58f9d5149
kjohn414-ford <kjohn414@ford.com>
2020-08-13 11:53:12 -0400

    added methods to configuration

--- MytestApplication1/app/src/main/java/com/fordmps/mobileapp/shared/configuration/Configuration.java
+++ MytestApplication1/app/src/main/java/com/fordmps/mobileapp/shared/configuration/Configuration.java
@@ -2,4 +2,25 @@ package com.fordmps.mobileapp.shared.configuration;
 
 public class Configuration {
 
+    private boolean paakEnabled = false;
+
+    public boolean isPaakEnabled() {
+        return paakEnabled;
+    }
+
+    public void setPaakEnabled(boolean paakEnabled) {
+        this.paakEnabled = paakEnabled;
+    }
+
+    public boolean isDashboardXApiPhase2Enabled() {
+        return false;
+    }
+
+    public boolean shouldShowFordScriptOverlay() {
+        return false;
+    }
+
+    public boolean isTmcMigrationEnabled() {
+        return false;
+    }
 }
